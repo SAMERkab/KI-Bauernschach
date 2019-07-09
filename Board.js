41758fbcac5fbab1daeefd0c72f13103973fc712
SAMERkab <samerkab2001@gmail.com>
2019-07-09 11:21:26 +0200

    bla bla

---
+++ Board.js
@@ -0,0 +1,56 @@
+class Board {
+    constructor(DOMTable, pawnTemplate, pawns) {
+        this.DOMTable = DOMTable;
+        this.pawnTemplate = pawnTemplate;
+        this.pawns = pawns;
+        this.setup();
+    }
+
+
+    setup() {
+        let pawnNode;
+        for (let pawn of this.pawns) {
+            pawnNode = this.pawnTemplate.content.cloneNode(true);
+            this.DOMTable.children[pawn.row].children[pawn.col].appendChild(pawnNode);
+        }
+    }
+
+
+    update(pawns) {
+        if (Array.isArray(pawns)) {
+            this.pawns = pawns;
+            this.setup();
+        } else {
+            let oldPawnNode = this.DOMTable.children[pawns.lastRow].children[pawns.lastCol].firstElementChild;
+            this.DOMTable.children[pawns.lastRow].children[pawns.lastCol].appendChild(oldPawnNode);
+        }
+    }
+
+
+    cellIsOccupied(row, col) {
+        return typeof this.DOMTable.children[row].children[col].firstElementChild !== "undefined";
+    }
+
+
+    highlightCell(row, col, pawn) {
+        let cell = this.DOMTable.children[row].children[col];
+        if (this.cellIsOccupied(row, col)) {
+            cell.classList.add("highlightedToAttack");
+            cell.addEventListener("click", () => {
+                this.emptyCell(row, col);
+                pawn.move(col - pawn.col);
+            });
+        } else {
+            cell.classList.add("highlightedToMove");
+            cell.addEventListener("click", () => {
+                pawn.move(col - pawn.col);
+            });
+        }
+    }
+
+
+    emptyCell(row, col) {
+        cell = this.DOMTable.children[row].children[col];
+        cell.removeChild(cell.firstElementChild);
+    }
+}
---
+++ Pawn.js
@@ -0,0 +1,46 @@
+class Pawn {
+    constructor(row, col, isAI) {
+        this.row = row;
+        this.col = col;
+        this.lastRow = null;
+        this.lastCol = null;
+        this.isAI = isAI;
+    }
+
+
+    move(dir) {
+        this.lastRow = this.row;
+        this.lastCol = this.col;
+        this.row += this.isAI ? -1 : 1;
+        this.col += dir;
+        board.update(this);
+    }
+
+
+    select() {
+        if (!this.isAI) {
+            let allowedMoves = this.getAllowedMoves();
+            for (let move of allowedMoves) {
+                board.highlightCell(this.row+1, this.col+move);
+            }
+        }
+    }
+
+
+    getAllowedMoves() {
+        let allowedMoves;
+
+        if (this.isAI) {
+
+        } else {
+            if ( !board.cellIsOccupied(this.row+1, this.col) )
+                allowedMoves.push(0);
+            if ( this.col > 0 && board.cellIsOccupied(this.row+1, this.col-1))
+                allowedMoves.push(-1);
+            if ( this.col < 2 && board.cellIsOccupied(this.row+1, this.col+1))
+                allowedMoves.push(1);
+        }
+
+        return allowedMoves;
+    }
+}
