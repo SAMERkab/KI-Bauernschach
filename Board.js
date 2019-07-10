8bd4ae536a5b906636a4147b34d097c4b77fdedb
SAMERkab <samerkab2001@outlook.de>
2019-07-10 10:16:43 +0200

    jsakdfjlkasd

--- Board.js
+++ Board.js
@@ -1,94 +1,105 @@
 class Board {
-    constructor(DOMTable, pawnTemplate, pawns) {
-        this.DOMTable = DOMTable;
-        this.pawnTemplate = pawnTemplate;
-        this.pawns = pawns;
-        this.setup();
-    }
+  constructor(DOMTable, pawnTemplate, pawns, rowsCount=3, colsCount=3) {
+    this.DOMTable = DOMTable;
+    this.pawnTemplate = pawnTemplate;
+    this.pawns = pawns;
+    this.rowsCount = rowsCount;
+    this.colsCount = colsCount;
+    this.setup();
+  }
 
 
-    setup() {
-        for (let pawn of this.pawns) {
-            this.addPawnNodeToCell(pawn);
-        }
-    }
-
-
-    addPawnNodeToCell(pawn) {
-        let pawnNode = this.pawnTemplate.content.cloneNode(true);
-        this.DOMTable.children[pawn.row].children[pawn.col].appendChild(pawnNode);
-        pawnNode = this.DOMTable.children[pawn.row].children[pawn.col].lastElementChild;
-        pawnNode.addEventListener("click", () => {
-            pawn.select();
-        });
-    }
-
-
-    update(pawns) {
-        if (Array.isArray(pawns)) {
-            this.pawns = pawns;
-            this.setup();
-        } else {
-            let pawn = pawns;
-            let oldPawnNode = this.DOMTable.children[pawn.lastRow].children[pawn.lastCol].firstElementChild;
-            oldPawnNode.parentNode.removeChild(oldPawnNode);
-            this.addPawnNodeToCell(pawn);
-        }
-    }
-
-
-    cellIsOccupied(row, col) {
-        return this.DOMTable.children[row].children[col].children.length > 0;
-    }
-
-
-    highlightCell(row, col, pawn) {
-        let cell = this.DOMTable.children[row].children[col];
+  setup() {
+    let cell, pawnNode;
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        cell = this.DOMTable.children[row].children[col];
         if (this.cellIsOccupied(row, col)) {
-            cell.classList.add("highlightedToAttack");
-            cell.addEventListener("click", () => {
-                this.emptyCell(row, col);
-                this.pawns = this.pawns.filter(pawn => pawn.row !== row && pawn.col !== col);
-                pawn.move(col - pawn.col);
-            });
-        } else {
-            cell.classList.add("highlightedToMove");
-            cell.addEventListener("click", () => {
-                pawn.move(col - pawn.col);
-            });
+          pawnNode = this.pawnTemplate.content.cloneNode(true);
+          cell.appendChild(pawnNode);
         }
+        cell.addEventListener("click", (event) => {
+          if (this.cellIsOccupied(row, col) && !this.pawns[row][col].isAI) {
+            this.pawns[row][col].select();
+          }
+        });
+      }
     }
+  }
 
 
-    resetHighlight() {
-        let cell;
-        for (let row = 0; row < 3; row++) {
-            for (let col = 0; col < 3; col++) {
-                cell = this.DOMTable.children[row].children[col];
-                cell.classList.remove("highlightedToMove", "highlightedToAttack");
-                this.removeAllListenersOfElem(cell);
-            }
+  update(pawns) {
+    if (Array.isArray(pawns)) {
+      this.pawns = pawns;
+      this.setup();
+    } else {
+      let pawn = pawns;
+      this.pawns[pawn.lastRow][pawn.lastCol] = null;
+      this.pawns[pawn.row][pawn.col] = pawn;
+      let oldPawnNode = this.DOMTable.children[pawn.lastRow].children[pawn.lastCol].firstElementChild;
+      this.DOMTable.children[pawn.row].children[pawn.col].appendChild(oldPawnNode);
+    }
+  }
+
+
+  cellIsOccupied(row, col) {
+    return this.pawns[row][col] !== null;
+  }
+
+
+  highlightCell(row, col, pawn) {
+    let cell = this.DOMTable.children[row].children[col];
+    let pawnCell = this.DOMTable.children[pawn.row].children[pawn.col];
+    this.resetHighlight();
+    pawn.isSelected = true;
+    if (this.cellIsOccupied(row, col)) {
+      cell.classList.add("highlightedToAttack");
+      pawnCell.classList.add("highlightedAttackingPawn");
+      cell.addEventListener("click", () => {
+        if (pawn.isSelected) {
+          this.emptyCell(row, col);
+          this.pawns[row][col] = null;
+          pawn.move(col - pawn.col);
         }
-    }
-
-
-    emptyCell(row, col) {
-        let cell = this.DOMTable.children[row].children[col];
-        cell.removeChild(cell.firstElementChild);
-    }
-
-
-    removeAllListenersOfElem(elem) {
-        let clone = elem.cloneNode(true);
-        elem.parentNode.replaceChild(clone, elem);
-    }
-
-
-    getPawnAt(row, col) {
-        for (let pawn of this.pawns) {
-            if (pawn.row === row && pawn.col === col)
-                return pawn;
+      });
+    } else {
+      cell.classList.add("highlightedToMove");
+      pawnCell.classList.add("highlightedMovingPawn");
+      cell.addEventListener("click", () => {
+        if (pawn.isSelected) {
+          pawn.move(col - pawn.col);
         }
-        return null;
+      });
     }
+  }
+
+
+  resetHighlight() {
+    let cell;
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        cell = this.DOMTable.children[row].children[col];
+        cell.classList.remove(
+          "highlightedToMove",
+          "highlightedToAttack",
+          "highlightedMovingPawn",
+          "highlightedAttackingPawn"
+        );
+        if (this.cellIsOccupied(row, col)) {
+          this.pawns[row][col].isSelected = false;
+        }
+      }
+    }
+  }
+
+
+  emptyCell(row, col) {
+    let cell = this.DOMTable.children[row].children[col];
+    cell.removeChild(cell.firstElementChild);
+  }
+
+
+  getPawnAt(row, col) {
+    return this.pawns[row][col];
+  }
 }
--- Pawn.js
+++ Pawn.js
@@ -1,47 +1,54 @@
 class Pawn {
-    constructor(row, col, isAI) {
-        this.row = row;
-        this.col = col;
-        this.lastRow = null;
-        this.lastCol = null;
-        this.isAI = isAI;
+  constructor(row, col, isAI) {
+    this.row = row;
+    this.col = col;
+    this.lastRow = null;
+    this.lastCol = null;
+    this.isAI = isAI;
+    this.isSelected = false;
+    this.badBoards = [];
+  }
+
+
+  move(dir) {
+    this.lastRow = this.row;
+    this.lastCol = this.col;
+    this.row += this.isAI ? 1 : -1;
+    this.col += dir;
+    board.resetHighlight();
+    board.update(this);
+  }
+
+
+  select() {
+    if (!this.isAI) {
+      let allowedMoves = this.getAllowedMoves();
+      for (let move of allowedMoves) {
+        board.highlightCell(this.row - 1, this.col + move, this);
+      }
+    }
+  }
+
+
+  getAllowedMoves() {
+    let allowedMoves = [];
+
+    if (this.isAI) {
+
+    } else {
+      if (!board.cellIsOccupied(this.row - 1, this.col))
+        allowedMoves.push(0);
+      if (this.col > 0 && board.cellIsOccupied(this.row - 1, this.col - 1) && board.getPawnAt(this.row - 1, this.col - 1).isAI)
+        allowedMoves.push(-1);
+      if (this.col < 2 && board.cellIsOccupied(this.row - 1, this.col + 1) && board.getPawnAt(this.row - 1, this.col + 1).isAI)
+        allowedMoves.push(1);
     }
 
-
-    move(dir) {
-        this.lastRow = this.row;
-        this.lastCol = this.col;
-        this.row += this.isAI ? 1 : -1;
-        this.col += dir;
-        board.resetHighlight();
-        board.update(this);
-    }
+    return allowedMoves;
+  }
 
 
-    select() {
-        if (!this.isAI) {
-            let allowedMoves = this.getAllowedMoves();
-            for (let move of allowedMoves) {
-                board.highlightCell(this.row-1, this.col+move, this);
-            }
-        }
-    }
+  isBadMove(dir) {
 
-
-    getAllowedMoves() {
-        let allowedMoves = [];
-
-        if (this.isAI) {
-
-        } else {
-            if ( !board.cellIsOccupied(this.row-1, this.col) )
-                allowedMoves.push(0);
-            if (this.col > 0 && board.cellIsOccupied(this.row-1, this.col-1) && board.getPawnAt(this.row-1, this.col-1).isAI)
-                allowedMoves.push(-1);
-            if (this.col < 2 && board.cellIsOccupied(this.row-1, this.col+1) && board.getPawnAt(this.row-1, this.col+1).isAI)
-                allowedMoves.push(1);
-        }
-
-        return allowedMoves;
-    }
+  }
 }
