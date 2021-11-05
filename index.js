ed413d711b435b31f1bf7c699e506c16a7076950
SAMERkab <samerkab2001@outlook.de>
2021-11-05 22:51:13 +0100

    No comment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const playerTurnFinishedEvent = new Event("playerTurnFinished");
 let DOMTable = document.getElementById("board");
 let playerPawnTemplate = document.getElementById("playerPawnTemplate");
 let AIPawnTemplate = document.getElementById("AIPawnTemplate");
-let pawnsObject, board, badBoards, isAITurn = false;
+let pawnsObject, board, badBoards, gameHasEnded, isAITurn = false;
 
 document.addEventListener("playerTurnFinished", () => {
   setTimeout(AIPlay, 250);
@@ -84,6 +84,7 @@ function checkGameFinished() {
 
 function playAgain() {
   isAITurn = false;
+  gameHasEnded = false;
   for (let row = 0; row < ROWS_COUNT; row++) {
     for (let col = 0; col < COLS_COUNT; col++) {
       board.emptyCell(row, col);
@@ -97,9 +98,31 @@ function playAgain() {
 
 
 function endGame(winner) {
-  if (winner == "player") {
-    badBoards.push(pawnsObject.generateIdForPawns(pawnsObject.lastAllPawns));
-    localStorage.setItem( "badBoards", JSON.stringify(badBoards) );
+  if (!gameHasEnded) {
+    gameHasEnded = true;
+    if (winner == "player") {
+      badBoards.push( pawnsObject.generateIdForPawns(pawnsObject.lastAllPawns) );
+      localStorage.setItem( "badBoards", JSON.stringify(badBoards) );
+    }
+    document.dispatchEvent( new CustomEvent("gameFinished", { detail: winner }) );
   }
-  document.dispatchEvent(new CustomEvent("gameFinished", { detail: winner }));
 }
+
+/*
+badBoards = [
+  " 01true  11true 20false  ",
+  " 01true02true10true 12false20false21false ",
+  "  02true10true11false12false 21false ", 
+  "  02true10true11false12true20false  ", 
+  " 01true 10false11true12true  22false", 
+  "00true  10true11false12true  22false", 
+  "00true 02true10false11true  21false22false", 
+  "00true01true 10false 12true 21false22false", 
+  " 01true 10true11true12false20false  ", 
+  "00true01true  11false12true20false 22false", 
+  "00true 02true 11true12false20false21false ", 
+  " 01true02true10true11false 20false 22false", 
+  " 01true  11true   22false", 
+  "   10true11false12true   "
+];
+*/
